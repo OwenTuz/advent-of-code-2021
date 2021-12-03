b222ee1327f9bf31ed0d7ef0bee2bfa6c4ef94f0
Owen Tuz <owen@automationlogic.com>
2021-12-03 23:52:43 +0000

    Day3: Part 2

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -22,8 +22,42 @@ fn part1(input: &Vec<&str>) -> u32 {
     return gamma * epsilon
 }
 
+enum RatingType {
+    Oxygen,
+    CO2,
+}
+
+fn find_rating(input: &Vec<u32>, rating_type: RatingType, pos: usize) -> u32 {
+    let (one, zero): (Vec<u32>, Vec<u32>) = input
+                      .iter()
+                      .partition(|x| (**x >> pos) & 1 == 1);
+    let next: &Vec<u32>;
+    match rating_type {
+        RatingType::Oxygen => {
+            next = if one.len() >= zero.len() { & one } else { &zero }
+        },
+        RatingType::CO2 => {
+            next = if one.len() < zero.len() { &one } else { &zero }
+        }
+    }
+    if next.len() == 1 {
+        return next[0]
+    }
+    return find_rating(next, rating_type, pos - 1)
+}
+
+fn part2(input: &Vec<&str>) -> u32 {
+    let initial_pos = input[0].len() - 1;
+    let input_b: Vec<u32> = input.iter()
+                                    .map(|x| u32::from_str_radix(x, 2).unwrap())
+                                    .collect();
+    let oxygen: u32 = find_rating(&input_b, RatingType::Oxygen, initial_pos);
+    let co2: u32 = find_rating(&input_b, RatingType::CO2, initial_pos);
+    return oxygen * co2
+}
+
 fn main() {
     let input: Vec<&str> = util::input_to_str_vec(include_str!("input"));
     println!("Part 1: Answer is {}", part1(&input));
-//    println!("Part 2: Answer is {}", part2(&input));
+    println!("Part 2: Answer is {}", part2(&input));
 }
