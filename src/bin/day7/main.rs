4f9e53e369a7f17fa2bd80b5b71acfd53145daeb
Owen Tuz <owen@automationlogic.com>
2021-12-07 14:41:09 +0000

    Day7: Cleanup: regretting choice of unsigned integer types

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -1,26 +1,22 @@
-fn part1(input: &Vec<usize>) -> usize {
-    let mut crabs: Vec<usize> = input.clone();
+fn part1(input: &Vec<isize>) -> isize {
+    let mut crabs: Vec<isize> = input.clone();
     crabs.sort();
     let median = crabs[crabs.len() / 2];
-    crabs.iter().fold(
-        0, |acc, x| if median >= *x { acc + (median - x) }
-                    else { acc + (x - median ) }
-    )
+    crabs.iter().fold(0, |acc, x| acc + (median - x).abs())
 }
 
-fn part2(input: &Vec<usize>) -> usize {
-    let mut crabs: Vec<usize> = input.clone();
+fn part2(input: &Vec<isize>) -> isize {
+    let mut crabs: Vec<isize> = input.clone();
     crabs.sort();
-    let sum: usize = crabs.iter().sum();
-    let mean: usize = sum / crabs.len();
+    let sum: isize = crabs.iter().sum();
+    let mean: isize = sum / crabs.len() as isize;
     crabs.iter().fold(
-        0, |acc, x| if mean >= *x { acc + ((mean - x)*((mean - x) + 1))/2 }
-                    else { acc + ((x - mean )*((x - mean) + 1))/2 }
+        0, |acc, x| acc + ((mean - x).abs() * ((mean - x).abs() + 1)) / 2
     )
 }
 
 fn main() {
-    let input: Vec<usize> = util::input_to_vec_t_fromstr(include_str!("input"), ',');
+    let input: Vec<isize> = util::input_to_vec_t_fromstr(include_str!("input"), ',');
     println!("Part 1: Answer is {}", part1(&input));
     println!("Part 2: Answer is {}", part2(&input));
 }
