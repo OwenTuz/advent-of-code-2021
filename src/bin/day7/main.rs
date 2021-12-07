68ec125fee24deb031b319b95f3c8d57889822d3
Owen Tuz <owen@automationlogic.com>
2021-12-07 14:35:55 +0000

    Day 7: Part 2

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -8,8 +8,19 @@ fn part1(input: &Vec<usize>) -> usize {
     )
 }
 
+fn part2(input: &Vec<usize>) -> usize {
+    let mut crabs: Vec<usize> = input.clone();
+    crabs.sort();
+    let sum: usize = crabs.iter().sum();
+    let mean: usize = sum / crabs.len();
+    crabs.iter().fold(
+        0, |acc, x| if mean >= *x { acc + ((mean - x)*((mean - x) + 1))/2 }
+                    else { acc + ((x - mean )*((x - mean) + 1))/2 }
+    )
+}
+
 fn main() {
     let input: Vec<usize> = util::input_to_vec_t_fromstr(include_str!("input"), ',');
     println!("Part 1: Answer is {}", part1(&input));
-//    println!("Part 2: Answer is {}", part2(&input));
+    println!("Part 2: Answer is {}", part2(&input));
 }
